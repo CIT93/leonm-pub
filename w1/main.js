7d5ffa6006a5e809f83ab4011e837d23dc7f6a7b
Leon Moua <leonmoua@gmail.com>
2024-09-03 15:44:26 -0700

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,21 @@
-console.log('Hello World!')
+console.log('hello from inside the main.js file')
+
+// let myVar;
+const myVar = "leon";
+
+const myVarType = typeof(myVar);
+
+console.log("myVarType" + myVarType);
+console.log(`myVarType ${myVarType}`);
+
+function runNow() {
+    if(myVarType === "number") {
+        console.log('this line will run');
+    }
+    else {
+        console.log('this line will not run');
+    }
+}
+
+runNow();
+runNow();
